4bbb0ed06f4cec6f6cbdbc4dc3b530bede235316
Leandro López (inkel) <inkel.ar@gmail.com>
2010-01-14 17:30:15 -0300

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :published, :title, :content
+  attr_accessible :published, :title, :content, :draft
+
+  validates_presence_of :title, :published, :content
 end
