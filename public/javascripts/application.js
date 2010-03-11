2c5d24797c0cf5c32f3a82e72fa3e94a6a6c673c
Leandro López (inkel) <inkel.ar@gmail.com>
2010-03-11 06:00:50 -0300

    Display photo title on hover

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(function() {
+        jQuery('ul.gallery a[title]').hover(
+                                            function() {
+                                                jQuery(this).append('<div class="title">' + this.title + '</div>');
+                                            },
+                                            function() {
+                                                jQuery(this).find('div.title').remove();
+                                            }
+                                            );
+    });
